2f387b68cb79c630c32c36c2eec92b67325f72ae
coco31821 <coco318213@gmail.com>
2026-07-07 11:22:16 +0900

    [FIX] 중복 콘텐츠 에러 코드 제거

--- src/main/java/com/sungjiduk/backend/common/constants/ErrorCode.java
+++ src/main/java/com/sungjiduk/backend/common/constants/ErrorCode.java
@@ -28,7 +28,6 @@ public enum ErrorCode {
 
     // Content쪽
     CONTENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 작품을 찾을 수 없습니다."),
-    CONTENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 콘텐츠를 찾을 수 없습니다.")
 
     // Spot쪽
     ANITABI_FETCH_FAILED(HttpStatus.BAD_GATEWAY, "Anitabi 성지 데이터를 불러오지 못했습니다."),
