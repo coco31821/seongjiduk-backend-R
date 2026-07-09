afb02ae5769c9f0e7c8daf4b54ce90d26df4704d
coco31821 <coco318213@gmail.com>
2026-07-09 09:05:57 +0900

    [FEAT] Redis cache 설정파일

---
+++ src/main/java/com/sungjiduk/backend/common/cache/CacheNames.java
@@ -0,0 +1,12 @@
+package com.sungjiduk.backend.common.cache;
+
+public final class CacheNames {
+    public static final String NEARBY_ATTRACTIONS = "nearbyAttractions";
+    public static final String STREET_VIEW = "streetView";
+    public static final String AI_DESCRIPTIONS = "aiDescriptions";
+    public static final String ROUTE_VERIFICATION = "routeVerification";
+    public static final String START_LOCATION = "startLocation";
+
+    private CacheNames() {
+    }
+}
